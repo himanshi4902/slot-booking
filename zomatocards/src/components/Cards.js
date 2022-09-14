ce4637280b73ef717c2688eb0bffb4716258cf9a
jahnvi15 <91893464+jahnvi15@users.noreply.github.com>
2022-09-14 22:24:25 +0530

    Added Book button and Changed cards format

--- zomatocards/src/components/Cards.js
+++ zomatocards/src/components/Cards.js
@@ -1,5 +1,10 @@
 import React from 'react'
 import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button';
+
+
+
+
 
 const Cards = ({ data }) => {
     return (
@@ -10,26 +15,29 @@ const Cards = ({ data }) => {
                         <>
                             <Card style={{ width: '22rem',border:"none" }} className="hove mb-4">
                                 <Card.Img variant="top" className='cd' src={element.imgdata} />
-
+                                
                                 <div className="card_body">
+                                    <div className='info'>
                                     <div className="upper_data d-flex justify-content-between align-items-center">
                                         <h4 className='mt-2'>{element.rname}</h4>
-
+                                        <span className = 'purple'><Button style={{color:"#00005c", margin: "5%", boxShadow: "5px 5px 3px rgba(46, 46, 46, 0.62)"}} as="input" type="button" value="Book"  />{' '}
+                                       
+                                        </span>
+                                        
+                                    </div>
                                     </div>
 
                                     <div className="lower_data d-flex  justify-content-between">
                                         <h5>{element.address}</h5>
                                         <span>{element.price}</span>
                                     </div>
-
+                                    
 
                                     <div className="extra"></div>
-
                                     <div className="last_data d-flex justify-content-between align-items-center">
-                                       
-                                        <p>{element.somedata}</p>
                                         
                                     </div>
+                                   
                                 </div>
 
                             </Card>
