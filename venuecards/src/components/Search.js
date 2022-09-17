b3954ff0f664a96b2234590850e3eee74dbf5277
jahnvi15 <91893464+jahnvi15@users.noreply.github.com>
2022-09-17 17:23:25 +0530

    progress-bar

--- venuecards/src/components/Search.js
+++ venuecards/src/components/Search.js
@@ -62,7 +62,20 @@ const Search = () => {
           </Navbar.Brand>
         </Container>
       </Navbar>
-    
+      <MDBRow>
+                    <div className="stepper-wrapper">
+                <div className="stepper-item completed">
+                <div className="step-counter">1</div>
+                <div className="step-name progress_label">Select Venue</div>
+                </div>
+                <div class="stepper-item active">
+        <div class="step-counter">2</div>
+        <div class="step-name">Slot Booking</div>
+      </div>
+                </div>
+            
+                
+            </MDBRow>
       <MDBRow sm="9" > 
             <div className="navbar_backspace d-flex justify-content-between align-items-center">
             </div>
